73666dd52bd7149205e42ece981b744ddfbf6a6f
Md.Ashraful Alam <ashrafulalam10000@gmail.com>
2022-06-28 09:46:45 +0600

    wall clock

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+const h = document.querySelector('.hour')
+const m = document.querySelector('.munite')
+const s = document.querySelector('.second')
+
+
+ const timeOutput = ()=>{
+    
+let time = new Date();
+let hour = time.getHours()
+let munite = time.getMinutes()
+let second = time.getSeconds()
+
+hour = (hour>12)? hour-12 : hour
+
+hour =(hour<10)? "0"+ hour : hour;
+munite =(munite<10)? "0"+ munite : munite;
+second =(second<10)? "0"+ second : second;
+
+ h.style.transform = `rotateZ(${hour*30+munite/12}deg)`
+ m.style.transform = `rotateZ(${munite*6}deg)`
+ s.style.transform = `rotateZ(${second*6}deg)`
+
+ }
+
+ setInterval(() => {
+    timeOutput()
+ }, 1000);
